be86bcf6ace08d301fd91100e30d568e72aaf8d2
Robert Rees <robert.rees@guardian.co.uk>
2015-02-21 22:53:18 +0000

    Creates a basic model stub

--- models.py
+++ models.py
@@ -3,3 +3,8 @@ from google.appengine.ext import ndb
 class Configuration(ndb.Model):
 	key = ndb.StringProperty()
 	value = ndb.StringProperty()
+
+class ContentSummary(ndb.Model):
+	url = ndb.StringProperty(required=True)
+	headline = ndb.StringProperty(required=True)
+	standfirst = ndb.TextProperty(required=True)
